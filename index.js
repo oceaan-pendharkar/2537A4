9e698109332445dbd2b9d20828b30ad2e8c73f7b
oceaan-pendharkar <oceaanpendharkar@gmail.com>
2023-05-16 11:04:00 -0700

    Step 10 Corner Case 3

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const setup = () => {
   let firstCard = undefined
   let secondCard = undefined
   $(".card").on(("click"), function () {
+    //check how many cards are flipped
+    let flippedCards = $(".flip").length
+    //if more than 2, return
+    if (flippedCards > 1) return
+
     $(this).toggleClass("flip");
 
     if (!firstCard)
@@ -25,6 +30,13 @@ const setup = () => {
         console.log("match")
         $(`#${firstCard.id}`).parent().off("click")
         $(`#${secondCard.id}`).parent().off("click")
+        //add .flip-paired class to both cards
+        $(`#${firstCard.id}`).parent().toggleClass("flip-paired")
+        $(`#${secondCard.id}`).parent().toggleClass("flip-paired")
+        //remove .flip class from both cards
+
+        $(`#${firstCard.id}`).parent().toggleClass("flip")
+        $(`#${secondCard.id}`).parent().toggleClass("flip")
         firstCard = undefined
       } else {
         console.log("no match")
